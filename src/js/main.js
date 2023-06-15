eec950ed680b054def3c73282dc5fea1dd48ed0d
Margaux Morat <98400662+gauxxmar@users.noreply.github.com>
2023-06-15 17:14:10 +0200

    commit du 15 juin

---
+++ src/js/main.js
@@ -0,0 +1,52 @@
+document.addEventListener("DOMContentLoaded", function () {
+  var accordionHeaders = document.querySelectorAll(".accordion-header");
+
+  accordionHeaders.forEach(function (header) {
+    header.addEventListener("click", function () {
+      var accordionItem = this.parentNode;
+      var content = accordionItem.querySelector(".accordion-content");
+
+      accordionItem.classList.toggle("active");
+
+      if (content.style.maxHeight) {
+        content.style.maxHeight = null;
+      } else {
+        content.style.maxHeight = content.scrollHeight + "px";
+      }
+    });
+  });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  var carouselTrack = document.querySelector(".carousel-track");
+  var carouselSlides = Array.from(document.querySelectorAll(".carousel-slide"));
+  var prevButton = document.querySelector(".carousel-prev");
+  var nextButton = document.querySelector(".carousel-next");
+
+  var slideWidth = carouselSlides[0].getBoundingClientRect().width;
+  var slideIndex = 0;
+
+  // Déplacer le carousel pour afficher le slide suivant
+  function moveToSlide(index) {
+    carouselTrack.style.transform = "translateX(-" + slideWidth * index + "px)";
+    slideIndex = index;
+  }
+
+  // Gestionnaire d'événement pour le bouton précédent
+  prevButton.addEventListener("click", function () {
+    slideIndex--;
+    if (slideIndex < 0) {
+      slideIndex = carouselSlides.length - 1;
+    }
+    moveToSlide(slideIndex);
+  });
+
+  // Gestionnaire d'événement pour le bouton suivant
+  nextButton.addEventListener("click", function () {
+    slideIndex++;
+    if (slideIndex >= carouselSlides.length) {
+      slideIndex = 0;
+    }
+    moveToSlide(slideIndex);
+  });
+});
